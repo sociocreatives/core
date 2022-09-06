69d849f3f27c9dca46f2bb5d1aa76ae13197fdbe
erick ayara <ayaraerick@gmail.com>
2022-09-06 13:24:36 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permissions.IsAuthenticated',]
